2a78608305b509b96a90ff21e5778ec22a3813d0
ahmad-imtiyaz <imtiyaznajih8@gmail.com>
2025-05-13 22:38:24 +0700

    Menambahkan semua file

---
+++ app/controllers/About.php
@@ -0,0 +1,13 @@
+<?php
+
+class About
+{
+    public function index($nama = 'Yazna', $pekerjaan = 'Programmer')
+    {
+        echo "Halo, nama saya $nama, pekerjaan saya $pekerjaan";
+    }
+    public function page()
+    {
+        echo 'About/page';
+    }
+}
