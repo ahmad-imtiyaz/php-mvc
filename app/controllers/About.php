b3fcdb4a5ca9a20871e7f0de1014160c655cb8a1
ahmad-imtiyaz <imtiyaznajih8@gmail.com>
2025-05-16 20:41:59 +0700

    menyelesaikan video ke 5, namun masih dengan error yang sama

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -7,11 +7,17 @@ class About extends Controller
         $data['nama'] = $nama;
         $data['pekerjaan'] = $pekerjaan;
         $data['umur'] = $umur;
+        $data['judul'] = 'About Me';
+        $this->view('templates/header', $data);
         $this->view('about/index', $data);
+        $this->view('templates/footer');
     }
 
     public function page()
     {
+        $data['judul'] = 'Page anjay';
+        $this->view('templates/header', $data);
         $this->view('about/page');
+        $this->view('templates/footer');
     }
 }
